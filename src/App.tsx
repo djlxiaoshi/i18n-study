0e11e2f8f2ac4596352a73664506c372e7f4a01b
lixiaolong <xiaolong.li@shopee.com>
2022-01-27 11:25:57 +0800

    first init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './i18n';
+import { useTranslation } from 'react-i18next';
+import i18n from 'i18next';
 
 function App() {
+  const { t } = useTranslation();
+  const toggle = () => {
+    i18n.changeLanguage('en')
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {t('name')}
+
+        <button onClick={toggle}>切换语言</button>
       </header>
     </div>
   );
---
+++ src/locales/index.ts
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-27 11:11:04
+ * @LastEditTime: 2022-01-27 11:22:18
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /i18n-study/src/locales/index.ts
+ */
+import en from './en.json';
+import zh from  './zh.json';
+
+ const resource = {
+    en: {
+        translation: en
+    },
+    zh: {
+        translation: zh
+    }
+}
+
+export default resource;
+
